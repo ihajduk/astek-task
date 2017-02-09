497d1a4e9db15b6bd1d684b02dd09c7e14d4df42
ihajduk <iwo.hajduk@gmail.com>
2017-02-09 17:55:41 +0100

    Added JAXB implementation for parsing

--- src/main/java/pl/parser/nbp/model/ExchangeType.java
+++ src/main/java/pl/parser/nbp/model/ExchangeType.java
@@ -1,6 +1,18 @@
 package pl.parser.nbp.model;
 
 public enum ExchangeType {
-    BUY,
-    SELL
+    BUY {
+        @Override
+        public String getRateValue(TableOfCurrencies.Position position) {
+            return position.getBid();
+        }
+    },
+    SELL{
+        @Override
+        public String getRateValue(TableOfCurrencies.Position position) {
+            return position.getSell();
+        }
+    };
+
+    public abstract String getRateValue(TableOfCurrencies.Position position);
 }
--- src/main/java/pl/parser/nbp/utils/XmlReaderUtils.java
+++ src/main/java/pl/parser/nbp/utils/XmlReaderUtils.java
@@ -2,30 +2,18 @@ package pl.parser.nbp.utils;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-import org.xml.sax.InputSource;
-import org.xml.sax.SAXException;
 import pl.parser.nbp.model.ExchangeType;
 import pl.parser.nbp.model.TableOfCurrencies;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.ws.spi.Provider;
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
 import java.io.StringReader;
 import java.math.BigDecimal;
 import java.text.ParseException;
 import java.util.List;
 import java.util.Optional;
-import java.util.function.Supplier;
 
 @Component
 public class XmlReaderUtils {
@@ -46,19 +34,14 @@ public class XmlReaderUtils {
             List<TableOfCurrencies.Position> positions = tableOfCurrencies.getPositions();
             Optional<TableOfCurrencies.Position> currencyTable = positions.stream().filter(t -> t.getCurrencyCode().equals(currency)).findFirst();
             TableOfCurrencies.Position filteredPosition = currencyTable.orElseThrow(() -> new JAXBException("Value for given currency is null - possibly XML document has changed"));
-            String output =
-                    xType.equals(ExchangeType.BUY) ? filteredPosition.getBid()
-                            : filteredPosition.getSell();
-
-            // TODO: czy wydzielać metody oraz wzorzec strategia na koniec
-            return parseUtils.parseValueToCurrency(output);
-
+            String outputAvgRate = xType.getRateValue(filteredPosition);
+            return parseUtils.parseValueToCurrency(outputAvgRate);
         } catch (JAXBException ex) {
             throw new RuntimeException("Could not parse XML: ", ex);
-        } catch (ParseException e) {
-            e.printStackTrace();
+        } catch (ParseException ex) {
+            throw new RuntimeException("Could not parse currency: ", ex);
         }
-
-        throw new RuntimeException("XML parsing could not be completed");
     }
+
+
 }
