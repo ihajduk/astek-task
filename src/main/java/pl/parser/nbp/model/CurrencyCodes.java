8cec18f4c57b1298b08348d1058df9960bc614c0
ihajduk <iwo.hajduk@gmail.com>
2017-02-20 16:42:34 +0100

    Swagger + validation enhancements

---
+++ src/main/java/pl/parser/nbp/model/CurrencyCodes.java
@@ -0,0 +1,5 @@
+package pl.parser.nbp.model;
+
+public abstract class CurrencyCodes {
+    public static transient final String ALLOWED_CURRENCIES = "EUR|USD|CHF|GBP";
+}
--- src/main/java/pl/parser/nbp/model/request/CurrencyStatsRequest.java
+++ src/main/java/pl/parser/nbp/model/request/CurrencyStatsRequest.java
@@ -1,13 +1,16 @@
 package pl.parser.nbp.model.request;
 
 import lombok.Data;
+import pl.parser.nbp.model.CurrencyCodes;
 
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
+import java.io.Serializable;
 
 @Data
-public class CurrencyStatsRequest {
-    @Pattern(regexp = "EUR|USD|CHF|GBP", message = "Wrong currency")
+public class CurrencyStatsRequest implements Serializable {
+
+    @Pattern(regexp = CurrencyCodes.ALLOWED_CURRENCIES, message = "Wrong currency")
     private String currencyCode;
     @NotNull
     private String startDateString;
