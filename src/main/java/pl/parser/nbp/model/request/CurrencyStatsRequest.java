b6ccf1b8b0a9f761be0474067205fad0c22a9d84
ihajduk <iwo.hajduk@gmail.com>
2017-03-13 22:07:53 +0100

    Proper service tests, WIP - refactor unit tests

--- src/main/java/pl/parser/nbp/model/request/CurrencyStatsRequest.java
+++ src/main/java/pl/parser/nbp/model/request/CurrencyStatsRequest.java
@@ -5,15 +5,14 @@ import pl.parser.nbp.model.CurrencyCodes;
 
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
-import java.io.Serializable;
 
 @Data
 public class CurrencyStatsRequest {
 
     @Pattern(regexp = CurrencyCodes.ALLOWED_CURRENCIES_PATTERN, message = "Wrong currency")
-    private String currencyCode;
+    private final String currencyCode;
     @NotNull
-    private String startDateString;
+    private final String startDateString;
     @NotNull
-    private String endDateString;
+    private final String endDateString;
 }
--- src/test/java/pl/parser/nbp/service/CurrencyRatesServiceImplTest.java
+++ src/test/java/pl/parser/nbp/service/CurrencyRatesServiceImplTest.java
@@ -1,18 +1,60 @@
 package pl.parser.nbp.service;
 
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.Spy;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+import pl.parser.nbp.model.ExchangeTypeHolder;
+import pl.parser.nbp.model.request.CurrencyStatsRequest;
+import pl.parser.nbp.model.response.CurrencyStatsResponse;
+import pl.parser.nbp.utils.ArithmeticUtils;
+import pl.parser.nbp.utils.ParseUtils;
+import pl.parser.nbp.utils.UrlUtils;
 
-import static org.junit.Assert.*;
+import java.math.BigDecimal;
+import java.time.LocalDate;
 
+@RunWith(MockitoJUnitRunner.class)
 public class CurrencyRatesServiceImplTest {
 
-    /**
-     * TODO: Should I mock the corresponding components or make acceptance tests for the entire service? Examples:
-     * TODO: 1. when parseUtils.parseStringToDate(startDate) thenReturn blabla, when parseString(endDate) thenReturn blabla2, ... etc
-     * TODO: 2. result = computeRateStatistics(1,2,3); assertEquals(result, 4);
-     * @throws Exception
-     */
+    @Mock
+    private ParseUtils parseUtils;
+    @Mock
+    private UrlUtils urlUtils;
+    @Mock
+    private ArithmeticUtils arithmeticUtils;
+    @InjectMocks
+    private CurrencyRatesServiceImpl currencyRatesService;
+
     @Test
     public void shouldComputeRatesStatistics() throws Exception {
+        //given
+        String dateString = "2015-03-17";
+        String currencyCode = "EUR";
+        LocalDate localDate = LocalDate.of(2015, 3, 17);
+        BigDecimal decimalBuy = BigDecimal.valueOf(4.0887);
+        BigDecimal decimalSell = BigDecimal.valueOf(4.1713);
+        CurrencyStatsRequest currencyStatsRequest = new CurrencyStatsRequest(currencyCode, dateString, dateString);
+        Mockito.when(parseUtils.parseStringToDate(dateString)).thenReturn(localDate);
+        Mockito.when(urlUtils.acquireAvgRateForCurrency(currencyCode, ExchangeTypeHolder.BUY, localDate)).thenReturn(decimalBuy);
+        Mockito.when(urlUtils.acquireAvgRateForCurrency(currencyCode, ExchangeTypeHolder.SELL, localDate)).thenReturn(decimalSell);
+        Mockito.when(arithmeticUtils.computeAvgRate(decimalBuy, decimalBuy)).thenReturn(decimalBuy);
+        Mockito.when(arithmeticUtils.computeAvgRate(decimalSell, decimalSell)).thenReturn(decimalSell);
+        Mockito.when(arithmeticUtils.computeStdDevRate(decimalSell, decimalSell, decimalSell)).thenReturn(BigDecimal.ZERO);
+        Mockito.when(parseUtils.formatValueToCurrencyString(decimalBuy)).thenReturn("4,0887");
+        Mockito.when(parseUtils.formatValueToCurrencyString(BigDecimal.ZERO)).thenReturn("0,0000");
+
+        //when
+        CurrencyStatsResponse currencyStatsResponse = currencyRatesService.computeRatesStatistics(currencyStatsRequest);
+
+        //then
+        Assert.assertEquals("4,0887", currencyStatsResponse.getAvgBuyingRate());
+        Assert.assertEquals("0,0000", currencyStatsResponse.getStdDevSellingRate());
     }
 }
